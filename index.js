0c98e62194fb63e22d218fae8124a3187bf38129
Juan Carlos <suker_juan@hotmail.com>
2024-08-28 17:58:40 +0200

    [ADD]  Phonebook backend step 8:  Configure morgan so that it also shows  the data sent in HTTP POST

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const express = require('express');
 const morgan = require('morgan')
 
 const PORT = 3001;
+console.log(`Server Running on port ${PORT}`)
 const app = express();
 
 app.use(express.json());
-app.use(morgan('tiny'))
+app.use(morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+      JSON.stringify(req.body),
+    ].join(' ')
+  }))
 
 let persons = [
 	{
