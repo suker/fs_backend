cc9c932e5187304bda2cea03061a7f5a160a038f
Juan Carlos <suker_juan@hotmail.com>
2024-09-02 14:51:23 +0200

    [ADD] 3.13: Phonebook database, step 1  Change the fetching of all phonebook entries  so that the data is fetched from the database

--- index.js
+++ index.js
@@ -1,54 +1,51 @@
+require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan')
+const app = express();
+const Contact = require('./models/Contact')
 
 const PORT = process.env.PORT || 3001
 console.log(`Server Running on port ${PORT}`)
-const app = express();
 
-// It serves static files 
+// MIDDLEWARES
+
+// use for static files of /dist dir
 app.use(express.static('dist'))
 
 app.use(express.json());
-app.use(morgan(function (tokens, req, res) {
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, 'content-length'), '-',
-      tokens['response-time'](req, res), 'ms',
-      JSON.stringify(req.body),
-    ].join(' ')
-  }))
+app.use(morgan('tiny'))
 
-let persons = [
-	{
-		id: '1',
-		name: 'Arto Hellas',
-		number: '040-123456',
-	},
-	{
-		id: '2',
-		name: 'Ada Lovelace',
-		number: '39-44-5323523',
-	},
-	{
-		id: '3',
-		name: 'Dan Abramov',
-		number: '12-43-234345',
-	},
-	{
-		id: '4',
-		name: 'Mary Poppendieck',
-		number: '39-23-6423122',
-	},
-];
+// let persons = [
+// 	{
+// 		id: '1',
+// 		name: 'Arto Hellas',
+// 		number: '040-123456',
+// 	},
+// 	{
+// 		id: '2',
+// 		name: 'Ada Lovelace',
+// 		number: '39-44-5323523',
+// 	},
+// 	{
+// 		id: '3',
+// 		name: 'Dan Abramov',
+// 		number: '12-43-234345',
+// 	},
+// 	{
+// 		id: '4',
+// 		name: 'Mary Poppendieck',
+// 		number: '39-23-6423122',
+// 	},
+// ];
 
 app.get('/', (request, response) => {
 	return response.send('Hello world!');
 });
 
 app.get('/api/persons', (request, response) => {
-	return response.json(persons);
+	Contact.find({}).then(contacts => {
+		response.json(contacts)
+	})
 });
 
 app.get('/api/persons/:id', (request, response) => {
